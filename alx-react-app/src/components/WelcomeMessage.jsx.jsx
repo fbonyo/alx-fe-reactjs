5622caae9e9903ef5b544c2a3e41f0b160309099
Fredrick Bonyo <fbonyo@gmail.com>
2025-10-29 21:44:59 +0300

    feat: Finalize Task 1 by correcting file extensions and structure

---
+++ alx-react-app/src/components/WelcomeMessage.jsx.jsx
@@ -0,0 +1,17 @@
+// src/components/WelcomeMessage.jsx
+
+function WelcomeMessage() {
+    return (
+        <div>
+            {/* The modified <h1> tag text */}
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            
+            <p>This is a simple JSX component.</p>
+            
+            {/* The new <p> tag */}
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
