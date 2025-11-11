8495f39979ea206c4c68c6a1da683175372e79a4
Fredrick Bonyo <fbonyo@gmail.com>
2025-11-11 17:19:19 +0300

    fix: move recipeStore.js to components directory as required

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useRecipeStore } from '../store/recipeStore'
+import { useRecipeStore } from './recipeStore'  // Change this line
 
 const AddRecipeForm = () => {
   const addRecipe = useRecipeStore((state) => state.addRecipe)
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import { useRecipeStore } from '../store/recipeStore'
+import { useRecipeStore } from './recipeStore'  // Change this line
 
 const Recipelist = () => {
   const recipes = useRecipeStore((state) => state.recipes)
