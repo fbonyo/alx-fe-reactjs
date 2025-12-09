43c69626a485b4e10f38cb6f4b5798ebbc32891e
Fredrick Bonyo <fbonyo@gmail.com>
2025-12-09 17:23:29 +0300

    Add form-handling-react assignment (controlled form + Formik)

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,17 @@
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
+
+function App() {
+  return (
+    <div>
+      <h1>Form Handling Assignment</h1>
+      <RegistrationForm />
+      <hr />
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
+
+
