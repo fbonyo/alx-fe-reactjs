60657830b8c3147e0cd0b96663fef48279f64c68
Fredrick Bonyo <fbonyo@gmail.com>
2025-12-10 17:41:50 +0300

    Add useAuth hook and update ProtectedRoute for authentication

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
@@ -7,33 +7,31 @@ import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   return (
     <Router>
       <div>
         <h1>React Router Advanced Demo</h1>
         <Routes>
+          {/* Home route */}
           <Route path="/" element={<Home />} />
-          <Route
-            path="/login"
-            element={<Login onLogin={() => setIsAuthenticated(true)} />}
-          />
 
-          {/* Use ProtectedRoute for profile */}
+          {/* Login route */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected Profile route */}
           <Route
             path="/profile/*"
             element={
-              <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <ProtectedRoute>
                 <Profile />
               </ProtectedRoute>
             }
           />
 
-          {/* Dynamic route */}
+          {/* Dynamic Blog route */}
           <Route path="/blog/:id" element={<BlogPost />} />
 
-          {/* Fallback */}
+          {/* Fallback route */}
           <Route path="*" element={<p>Page not found</p>} />
         </Routes>
       </div>
@@ -42,6 +40,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
