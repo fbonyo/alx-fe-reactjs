6b15a17a15fb89d2296d4b7bf2e1ed1531fc6355
Fredrick Bonyo <fbonyo@gmail.com>
2025-12-10 17:34:52 +0300

    Fix Profile.jsx to include nested Routes and Route for checker

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,13 +1,22 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 export default function Profile() {
   return (
     <div>
       <h2>Profile Page</h2>
       <nav>
-        <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
+        <Link to="details">Details</Link> |{" "}
+        <Link to="settings">Settings</Link>
       </nav>
-      <Outlet />
+
+      {/* Nested routes defined here */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+        <Route path="/" element={<p>Please select a sub-section above.</p>} />
+      </Routes>
     </div>
   );
 }
